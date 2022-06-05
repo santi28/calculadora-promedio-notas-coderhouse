effffb18eb505c4c08aaefe4196d3e25db4c32d8
Santiago de Nicolás <santidenicolas@gmail.com>
2022-06-05 16:28:10 -0300

    :tada: Inint new repo

---
+++ README.md
@@ -0,0 +1,35 @@
+# Calculadora de promedios
+## Que hace esta calculadora
+Esta calculadora, calcula los promedios de dos alumnos *(Juan y Valentin)*.
+
+Ellos actualmente cursan 3 materias: *Matemática, Geografía y Física*. Cada una tiene una calificación del 0 al 10 que se reparten en 4 cuatrimestres por materia.
+
+Si el alumno saca un promedio mayor o igual a 6, entonces se le otorga la calificación de *Aprobado*. Si no, se le otorga la calificación de *Recursa*.
+
+Una vez terminada la carga de datos, se muestra en una alerta la calificación de cada alumno, su promedio y si este abrobó o recursa esta materia.
+
+## Datos de prueba
+La calculadora ha sido probada con los siguentes datos de prueba que pueden ser usados para tener una idea de como funciona y los resultados que esta deberia arrojar.
+
+_**Notas de prueba para Juan:**_
+|            | 1° | 2° | 3° | 4° |
+|------------|----|----|----|----|
+| Matematica | 10 | 10 | 9  | 8  |
+| Fisica     | 10 | 9  | 7  | 4  |
+| Quimica    | 1  | 3  | 2  | 6  |
+_Resultados:_
+_**Matemática:** 9.25 (Aprobado)_
+_**Geografía:** 7.5 (Apobado)_
+_**Física:** 3 (Recursa)_
+
+_**Notas de prueba para Valentin:**_
+|            | 1° | 2° | 3° | 4° |
+|------------|----|----|----|----|
+| Matematica | 5  | 7  | 4  | 6  |
+| Fisica     | 8  | 7  | 7  | 8  |
+| Quimica    | 6  | 4  | 6  | 5  |
+
+_Resultados:_
+_**Matemática:** 5.5 (Recursa)_
+_**Geografía:** 7.5 (Apobado)_
+_**Física:** 5.25 (Recursa)_
---
+++ main.js
@@ -0,0 +1,89 @@
+alert("Calculadora de promedios\n\nEn esta calculadora podrás cargar las notas de dos alumnos en las materias (Geografía, Matematica y Fisica). Cada materia tiene una nota de 0 a 10.\nCada materia tiene 4 cuatrimestres los cuales se promediaran.\nSi el alumno obtiene un promedio final mayor de 6, aprueba la materia, en caso contrario la desaprobara\n\nAl final, el sistema retornara, por cada alumno, el promedio de cada materia y si esta fue aprobada o no\nTambien se retornara la cantidad de alumnos que hayan aprobado dicha materia\n\nEjemplo:\n Laura\n  Geografía: Aprobada (Promedio de 8.5)\n  Matemática: Desaprobada (Promedio de 2.75)")
+
+function Alumno (name, mathAverage, geographyAverage, physicsAverage) {
+  this.name = name
+  this.math = mathAverage
+  this.geography = geographyAverage
+  this.physics = physicsAverage
+
+  /** 
+   * @param {'math'|'geography'|'physics'} subject 
+   */
+  this.isApproved = function (subject) {
+    const average = this[subject]
+
+    if (average >= 6) {
+      return "Aprobado"
+    } else {
+      return "Recursa"
+    }
+  }
+}
+
+function loadNotes (student, subject) {
+  let noteSumRestult = 0
+
+  for (var i = 0; i < 4; i++) {
+    let note = parseInt(prompt(`${student}\nIngrese la nota para ${subject} en el cuatrimestre ${i + 1}`))
+    while (isNaN(note) || note < 0 || note > 10) {
+      note = parseInt(prompt(`${student}\nIngrese una nota valida para ${subject} en el cuatrimestre ${i + 1}`))
+    }
+
+    noteSumRestult += note
+  }
+
+  return noteSumRestult / 4
+}
+
+
+const juan = new Alumno('Juan')
+juan.math = loadNotes(juan.name, "Matemática")
+juan.geography = loadNotes(juan.name, "Geografía")
+juan.physics = loadNotes(juan.name, "Física")
+console.dir({
+  nombre: juan.name,
+  matematica: {
+    promedio: juan.math,
+    aprobado: juan.isApproved('math')
+  },
+  geografia: {
+    promedio: juan.geography,
+    aprobado: juan.isApproved('geography')
+  },
+  fisica: {
+    promedio: juan.physics,
+    aprobado: juan.isApproved('physics')
+  }
+})
+
+const valentin = new Alumno('Valentin')
+valentin.math = loadNotes(valentin.name, "Matemática")
+valentin.geography = loadNotes(valentin.name, "Geografía")
+valentin.physics = loadNotes(valentin.name, "Física")
+console.dir({
+  nombre: valentin.name,
+  matematica: {
+    promedio: valentin.math,
+    aprobado: valentin.isApproved('math')
+  },
+  geografia: {
+    promedio: valentin.geography,
+    aprobado: valentin.isApproved('geography')
+  },
+  fisica: {
+    promedio: valentin.physics,
+    aprobado: valentin.isApproved('physics')
+  }
+})
+
+alert(`
+Alumnos:
+  ${juan.name}:
+    Matemática: ${juan.math} (${juan.isApproved('math')})
+    Geografía: ${juan.geography} (${juan.isApproved('geography')})
+    Física: ${juan.physics} (${juan.isApproved('physics')})
+  ${valentin.name}:
+    Matemática: ${valentin.math} (${valentin.isApproved('math')})
+    Geografía: ${valentin.geography} (${valentin.isApproved('geography')})
+    Física: ${valentin.physics} (${valentin.isApproved('physics')})
+`)
