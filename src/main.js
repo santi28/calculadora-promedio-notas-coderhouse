ae9d7dafab06602e93c04e4c84230f8e1213e098
Santiago de Nicolás <santidenicolas@gmail.com>
2022-06-11 02:21:35 -0300

    :art: Added arrays and code refactor

--- src/main.js
+++ src/main.js
@@ -1,88 +1,87 @@
 alert('Calculadora de promedios\n\nEn esta calculadora podrás cargar las notas de dos alumnos en las materias (Geografía, Matematica y Fisica). Cada materia tiene una nota de 0 a 10.\nCada materia tiene 4 cuatrimestres los cuales se promediaran.\nSi el alumno obtiene un promedio final mayor de 6, aprueba la materia, en caso contrario la desaprobara\n\nAl final, el sistema retornara, por cada alumno, el promedio de cada materia y si esta fue aprobada o no\nTambien se retornara la cantidad de alumnos que hayan aprobado dicha materia\n\nEjemplo:\n Laura\n  Geografía: Aprobada (Promedio de 8.5)\n  Matemática: Desaprobada (Promedio de 2.75)')
 
-function Alumno (name, mathAverage, geographyAverage, physicsAverage) {
+function Student (name) {
+  // Variables con valor inicial variable
   this.name = name
-  this.math = mathAverage
-  this.geography = geographyAverage
-  this.physics = physicsAverage
 
-  /**
-   * @param {'math'|'geography'|'physics'} subject
+  // Variables con valor inicial predefinido
+  this.math = []
+  this.physics = []
+  this.geography = []
+
+  /** Obtiene la calificación promedio y condicion de aprobación de una materia dada
+   * @param {number[]} subject
    */
-  this.isApproved = function (subject) {
-    const average = this[subject]
+  this.getAverage = function (subject) {
+    let average = 0
+    for (let i = 0; i < subject.length; i++) {
+      // Obtiene la suma de todas las notas
+      average += subject[i]
+    }
 
-    if (average >= 6) {
-      return 'Aprobado'
-    } else {
-      return 'Recursa'
+    // Obtiene el promedio de la suma de todas las notas
+    average /= subject.length
+
+    // En caso de que el promedio sea mayor a 6, el alumno aprobó la materia
+    const approved = (average >= 6)
+
+    return { average, approved }
+  }
+
+  /** Obtiene el promedio y la condicion de aprobacion para todas las materias
+   * @param {'math'|'geography'|'physics'} subject
+   * @param {number} note
+   */
+  this.getAverageCalification = function () {
+    const mathAverage = this.getAverage(this.math)
+    const geographyAverage = this.getAverage(this.geography)
+    const physicsAverage = this.getAverage(this.physics)
+
+    return {
+      math: mathAverage,
+      geography: geographyAverage,
+      physics: physicsAverage
     }
   }
 }
 
-function loadNotes (student, subject) {
-  let noteSumRestult = 0
-
+function loadCalifications (subject, name) {
+  const califications = []
   for (let i = 0; i < 4; i++) {
-    let note = parseInt(prompt(`${student}\nIngrese la nota para ${subject} en el cuatrimestre ${i + 1}`))
-    while (isNaN(note) || note < 0 || note > 10) {
-      note = parseInt(prompt(`${student}\nIngrese una nota valida para ${subject} en el cuatrimestre ${i + 1}`))
+    let calification = parseInt(prompt(`${name}\nIngrese la nota para ${subject} en el cuatrimestre ${i + 1}`))
+    while (isNaN(calification) || calification < 0 || calification > 10) {
+      calification = parseInt(prompt(`${name}\nIngrese una nota valida para ${subject} en el cuatrimestre ${i + 1}`))
     }
 
-    noteSumRestult += note
+    califications.push(calification)
   }
 
-  return noteSumRestult / 4
+  return califications
 }
 
-const juan = new Alumno('Juan')
-juan.math = loadNotes(juan.name, 'Matemática')
-juan.geography = loadNotes(juan.name, 'Geografía')
-juan.physics = loadNotes(juan.name, 'Física')
-console.dir({
-  nombre: juan.name,
-  matematica: {
-    promedio: juan.math,
-    aprobado: juan.isApproved('math')
-  },
-  geografia: {
-    promedio: juan.geography,
-    aprobado: juan.isApproved('geography')
-  },
-  fisica: {
-    promedio: juan.physics,
-    aprobado: juan.isApproved('physics')
-  }
-})
+function createStudentWithCalifications (name) {
+  const student = new Student(name)
+  student.math = loadCalifications('Matemática', name)
+  student.geography = loadCalifications('Geografía', name)
+  student.physics = loadCalifications('Física', name)
 
-const valentin = new Alumno('Valentin')
-valentin.math = loadNotes(valentin.name, 'Matemática')
-valentin.geography = loadNotes(valentin.name, 'Geografía')
-valentin.physics = loadNotes(valentin.name, 'Física')
-console.dir({
-  nombre: valentin.name,
-  matematica: {
-    promedio: valentin.math,
-    aprobado: valentin.isApproved('math')
-  },
-  geografia: {
-    promedio: valentin.geography,
-    aprobado: valentin.isApproved('geography')
-  },
-  fisica: {
-    promedio: valentin.physics,
-    aprobado: valentin.isApproved('physics')
-  }
-})
+  return student
+}
+
+const juan = createStudentWithCalifications('Juan')
+const juanCalifications = juan.getAverageCalification()
+
+const valentin = createStudentWithCalifications('Valentin')
+const valentinCalifications = valentin.getAverageCalification()
 
 alert(`
 Alumnos:
   ${juan.name}:
-    Matemática: ${juan.math} (${juan.isApproved('math')})
-    Geografía: ${juan.geography} (${juan.isApproved('geography')})
-    Física: ${juan.physics} (${juan.isApproved('physics')})
+    Matemática: ${juanCalifications.math.average} (${juanCalifications.math.approved})
+    Geografía: ${juanCalifications.geography.average} (${juanCalifications.geography.approved})
+    Física: ${juanCalifications.physics.average} (${juanCalifications.physics.approved})
   ${valentin.name}:
-    Matemática: ${valentin.math} (${valentin.isApproved('math')})
-    Geografía: ${valentin.geography} (${valentin.isApproved('geography')})
-    Física: ${valentin.physics} (${valentin.isApproved('physics')})
+    Matemática: ${valentinCalifications.math.average} (${valentinCalifications.math.approved})
+    Geografía: ${valentinCalifications.geography.average} (${valentinCalifications.geography.approved})
+    Física: ${valentinCalifications.physics.average} (${valentinCalifications.physics.approved})
 `)
