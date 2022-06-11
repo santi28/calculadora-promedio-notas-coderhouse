c537967c8cb4e71fa0c8ac288584acb62ed9a3ed
Santiago de Nicolás <santidenicolas@gmail.com>
2022-06-10 22:37:48 -0300

    :rotating_light: Fixed lintern warns

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-alert("Calculadora de promedios\n\nEn esta calculadora podrás cargar las notas de dos alumnos en las materias (Geografía, Matematica y Fisica). Cada materia tiene una nota de 0 a 10.\nCada materia tiene 4 cuatrimestres los cuales se promediaran.\nSi el alumno obtiene un promedio final mayor de 6, aprueba la materia, en caso contrario la desaprobara\n\nAl final, el sistema retornara, por cada alumno, el promedio de cada materia y si esta fue aprobada o no\nTambien se retornara la cantidad de alumnos que hayan aprobado dicha materia\n\nEjemplo:\n Laura\n  Geografía: Aprobada (Promedio de 8.5)\n  Matemática: Desaprobada (Promedio de 2.75)")
+alert('Calculadora de promedios\n\nEn esta calculadora podrás cargar las notas de dos alumnos en las materias (Geografía, Matematica y Fisica). Cada materia tiene una nota de 0 a 10.\nCada materia tiene 4 cuatrimestres los cuales se promediaran.\nSi el alumno obtiene un promedio final mayor de 6, aprueba la materia, en caso contrario la desaprobara\n\nAl final, el sistema retornara, por cada alumno, el promedio de cada materia y si esta fue aprobada o no\nTambien se retornara la cantidad de alumnos que hayan aprobado dicha materia\n\nEjemplo:\n Laura\n  Geografía: Aprobada (Promedio de 8.5)\n  Matemática: Desaprobada (Promedio de 2.75)')
 
 function Alumno (name, mathAverage, geographyAverage, physicsAverage) {
   this.name = name
@@ -6,16 +6,16 @@ function Alumno (name, mathAverage, geographyAverage, physicsAverage) {
   this.geography = geographyAverage
   this.physics = physicsAverage
 
-  /** 
-   * @param {'math'|'geography'|'physics'} subject 
+  /**
+   * @param {'math'|'geography'|'physics'} subject
    */
   this.isApproved = function (subject) {
     const average = this[subject]
 
     if (average >= 6) {
-      return "Aprobado"
+      return 'Aprobado'
     } else {
-      return "Recursa"
+      return 'Recursa'
     }
   }
 }
@@ -23,7 +23,7 @@ function Alumno (name, mathAverage, geographyAverage, physicsAverage) {
 function loadNotes (student, subject) {
   let noteSumRestult = 0
 
-  for (var i = 0; i < 4; i++) {
+  for (let i = 0; i < 4; i++) {
     let note = parseInt(prompt(`${student}\nIngrese la nota para ${subject} en el cuatrimestre ${i + 1}`))
     while (isNaN(note) || note < 0 || note > 10) {
       note = parseInt(prompt(`${student}\nIngrese una nota valida para ${subject} en el cuatrimestre ${i + 1}`))
@@ -35,11 +35,10 @@ function loadNotes (student, subject) {
   return noteSumRestult / 4
 }
 
-
 const juan = new Alumno('Juan')
-juan.math = loadNotes(juan.name, "Matemática")
-juan.geography = loadNotes(juan.name, "Geografía")
-juan.physics = loadNotes(juan.name, "Física")
+juan.math = loadNotes(juan.name, 'Matemática')
+juan.geography = loadNotes(juan.name, 'Geografía')
+juan.physics = loadNotes(juan.name, 'Física')
 console.dir({
   nombre: juan.name,
   matematica: {
@@ -57,9 +56,9 @@ console.dir({
 })
 
 const valentin = new Alumno('Valentin')
-valentin.math = loadNotes(valentin.name, "Matemática")
-valentin.geography = loadNotes(valentin.name, "Geografía")
-valentin.physics = loadNotes(valentin.name, "Física")
+valentin.math = loadNotes(valentin.name, 'Matemática')
+valentin.geography = loadNotes(valentin.name, 'Geografía')
+valentin.physics = loadNotes(valentin.name, 'Física')
 console.dir({
   nombre: valentin.name,
   matematica: {
