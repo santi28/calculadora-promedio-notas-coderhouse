cb0fe0891dfddd66daf68f32dbfee28626ba571a
Santiago de Nicolás <santidenicolas@gmail.com>
2022-06-17 10:34:03 -0300

    :sparkles: Print results in table foreach student and subject

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
+const debugMode = false
+if (debugMode) alert('ATENCIÓN: El modo de depuración está activado.\nLas calificaciones se cargaran automáticamente de manera aleatoria.')
+
 alert('Calculadora de promedios\n\nEn esta calculadora podrás cargar las notas de dos alumnos en las materias (Geografía, Matematica y Fisica). Cada materia tiene una nota de 0 a 10.\nCada materia tiene 4 cuatrimestres los cuales se promediaran.\nSi el alumno obtiene un promedio final mayor de 6, aprueba la materia, en caso contrario la desaprobara\n\nAl final, el sistema retornara, por cada alumno, el promedio de cada materia y si esta fue aprobada o no\nTambien se retornara la cantidad de alumnos que hayan aprobado dicha materia\n\nEjemplo:\n Laura\n  Geografía: Aprobada (Promedio de 8.5)\n  Matemática: Desaprobada (Promedio de 2.75)')
+
+const diccionarioDeMaterias = {
+  math: 'Matemática',
+  geography: 'Geografía',
+  physics: 'Física'
+}
 
 function Student (name) {
   // Variables con valor inicial variable
@@ -9,8 +18,17 @@ function Student (name) {
   this.physics = []
   this.geography = []
 
+  /** Retorna todas las calificaciones en una lista */
+  this.getCalifications = function () {
+    return [
+      { subject: 'math', califications: this.math },
+      { subject: 'geography', califications: this.geography },
+      { subject: 'physics', califications: this.physics }
+    ]
+  }
+
   /** Obtiene la calificación promedio y condicion de aprobación de una materia dada
-   * @param {number[]} subject
+   * @param {number[]} subject Array de notas de una materia
    */
   this.getAverage = function (subject) {
     let average = 0
@@ -48,6 +66,14 @@ function Student (name) {
 function loadCalifications (subject, name) {
   const califications = []
   for (let i = 0; i < 4; i++) {
+    // Carga de calificaciones de prueba mediante metodo Math, para que no se tengan que cargar manualmente en desarrollo
+    if (debugMode) {
+      const randomCalification = Math.floor(Math.random() * 10) + 1
+      console.log(`Califiación de ${name} en ${subject} en cuatrimestre ${i + 1}: ${randomCalification}`)
+      califications.push(randomCalification)
+      continue // Termina la iteración actual y continua con la siguiente
+    }
+
     let calification = parseInt(prompt(`${name}\nIngrese la nota para ${subject} en el cuatrimestre ${i + 1}`))
     while (isNaN(calification) || calification < 0 || calification > 10) {
       calification = parseInt(prompt(`${name}\nIngrese una nota valida para ${subject} en el cuatrimestre ${i + 1}`))
@@ -68,17 +94,40 @@ function createStudentWithCalifications (name) {
   return student
 }
 
-function printCalificationsOnTable (tableID, studentCalifications) {
+/** Imprime en la tabla deseada la información del alumno
+ * @param {string} tableID
+ * @param {Student} student
+ */
+function printCalificationsOnTable (tableID, student) {
   const table = document.getElementById(tableID)
+
+  // Obtiene las calificaciones de cada materia
+  const subjects = student.getCalifications()
+
+  const subjectsRows = subjects.map(subject => {
+    const { approved, average } = student.getAverage(subject.califications)
+    const subjectNaturalName = diccionarioDeMaterias[subject.subject]
+
+    const tableRow = document.createElement('tr')
+    tableRow.innerHTML = `
+      <td>${subjectNaturalName}</td>
+      ${subject.califications.map(calification => `<td>${calification}</td>`).join('')}
+      <td>${approved ? 'Aprobado' : 'Desaprobado'}</td>
+      <td>${average}</td>
+    `
+
+    return tableRow
+  })
+
+  table.innerHTML = subjectsRows.map(row => row.outerHTML).join('')
 }
 
 const juan = createStudentWithCalifications('Juan')
-const juanCalifications = juan.getAverageCalification()
 
-const valentin = createStudentWithCalifications('Valentin')
-const valentinCalifications = valentin.getAverageCalification()
+// const valentin = createStudentWithCalifications('Valentin')
+// const valentinCalifications = valentin.getAverageCalification()
 
-printCalificationsOnTable('resultadosJuan', juanCalifications)
+printCalificationsOnTable('resultadosJuan', juan)
 
 // alert(`
 // Alumnos:
